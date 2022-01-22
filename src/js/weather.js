3d6b28f29bccf50a634d51711f7637be0d97a71c
Do-Hoon Kim <k99hoon@gmail.com>
2022-01-22 23:05:22 +0900

    Change typo in weather.js

--- src/js/weather.js
+++ src/js/weather.js
@@ -3,8 +3,9 @@ const API_key = "861fac13a49c0c217480148a03618c35";
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  //console.log(lat, lng);
+
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_key}&units=metric`;
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
