339ec944eb598a9d3950a08c2c2cb7bf916b946f
Do-Hoon Kim <k99hoon@gmail.com>
2022-01-22 22:52:03 +0900

    Add Weather announcement function

---
+++ src/js/weather.js
@@ -0,0 +1,22 @@
+const API_key = "861fac13a49c0c217480148a03618c35";
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  //console.log(lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_key}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = `${data.main.temp}ºC @ ${data.name}`;
+      weather.innerText = `${data.weather[0].main}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
