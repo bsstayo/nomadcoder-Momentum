92eac1b1dfedca2212891ee34f169c674402811d
Do-Hoon Kim <k99hoon@gmail.com>
2022-01-23 23:37:10 +0900

    Change background image path

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,7 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const IMG_WAY = `./../../img/${chosenImage}`;
+//https://bsstayo.github.io/nomadcoder-Momentum/img/0.jpg
 
-document.body.style.backgroundImage = `url(${IMG_WAY})`;
+//document.body.style.backgroundImage = `url(${IMG_WAY})`;
+document.body.style.backgroundImage = `url(https://bsstayo.github.io/nomadcoder-Momentum/img/${chosenImage})`;
